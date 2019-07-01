8354c2ff4123a4327725e4f77f5c86a548233b96
Anika <radianika@gmail.com>
2019-07-02 00:09:33 +0300

    Updates

---
+++ precache-manifest.bc09307febbd10fd971c8e54578bf00a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8651f308c966b79249c35175affea0",
+    "url": "/React-static-todoApp/index.html"
+  },
+  {
+    "revision": "fbab1b7865217034062d",
+    "url": "/React-static-todoApp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/React-static-todoApp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "fbab1b7865217034062d",
+    "url": "/React-static-todoApp/static/js/main.752f9914.chunk.js"
+  },
+  {
+    "revision": "13f6c73e84b28a5a1fd6",
+    "url": "/React-static-todoApp/static/js/runtime~main.e376aa0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-static-todoApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
