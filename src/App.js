31b0ef0674ea184dcbb3ef7b179360d8aa838744
Anika <radianika@gmail.com>
2019-07-02 16:21:17 +0300

    Styles for app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h1 className="title">todos</h1>
+        <input type="text" className="input" placeholder="What needs to be done?"/>
       </header>
+      <main>
+        <ul className="list">
+          <li className="list__item"><label><input className="checkbox" type="checkbox" />First</label> <button className="btn delete-btn" type="button">{String.fromCharCode(10005)}</button></li>
+          <li className="list__item"><label><input className="checkbox" type="checkbox" />Second</label><button className="btn delete-btn" type="button">{String.fromCharCode(10005)}</button></li>
+          <li className="list__item"><label><input className="checkbox" type="checkbox" />Third</label><button className="btn delete-btn" type="button">{String.fromCharCode(10005)}</button></li>
+        </ul>
+      </main>
+      <footer className="nav">
+        <p> 3 items left</p>
+        <div>
+          <button className="btn nav__btn" type="button">All</button>
+          <button className="btn nav__btn" type="button">Active</button>
+          <button className="btn nav__btn" type="button">Complited</button>
+        </div>
+        <button className="btn" type="button">Clear complited</button>
+      </footer>
     </div>
   );
 }
